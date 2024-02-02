b6761c734c44ad805d59d1728f84e494714f5168
Simiams <simon.convert@epsi.fr>
2024-02-02 17:32:03 +0100

    first commit

---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {test: "test"}
+  }
+}
+</script>
+
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HelloWorldOptionApi.vue
@@ -0,0 +1,16 @@
+<script>
+import HelloComponent from "@/components/HelloComponent.vue";
+
+export default {
+  name: 'HelloWorldOptionsApi',
+  components: {HelloComponent},
+  data() {
+    return {test: "ma donnée de test"}
+  },
+  methods: {}
+}
+</script>
+
+<template>
+  <HelloComponent msg="TESTEST"/>
+</template>
